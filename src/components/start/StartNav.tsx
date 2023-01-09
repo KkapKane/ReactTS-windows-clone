b62eac7cf43aa1b4250e60678a28807acf11135e
Kane <LisaxKane@gmail.com>
2023-01-08 22:51:14 -0600

    added start-nav inside start-panel and made folders to organize the start components

---
+++ src/components/start/StartNav.tsx
@@ -0,0 +1,54 @@
+import { AiOutlinePicture, AiOutlinePoweroff } from "react-icons/ai";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { BiFileBlank } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+import { CiSettings } from "react-icons/ci";
+
+interface Props {
+  isHover: boolean;
+  isClicked: boolean;
+  setIsHover: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function StartNav({ isClicked, isHover, setIsHover }: Props) {
+  return (
+    <div
+      className='start-navBar'
+      onMouseOver={() => setIsHover(true)}
+      onMouseOut={() => setIsHover(false)}
+      style={
+        isHover
+          ? { width: "50%", boxShadow: "3px 0px 6px 0px gray " }
+          : { width: "60px" }
+      }
+    >
+      <div id='start-hamburger'>
+        <RxHamburgerMenu size={22} />
+        {isHover ? <span>START</span> : null}
+      </div>
+      <div className='start-bottomGroup'>
+        <div className='start-utility'>
+          <CgProfile size={21} />
+          {isHover ? <span>Profile</span> : null}
+        </div>
+        <div className='start-utility'>
+          <BiFileBlank size={21} />
+          {isHover ? <span>Documents</span> : null}
+        </div>
+
+        <div className='start-utility'>
+          <AiOutlinePicture size={21} />
+          {isHover ? <span>Pictures</span> : null}
+        </div>
+        <div className='start-utility'>
+          <CiSettings size={21} />
+          {isHover ? <span>Settings</span> : null}
+        </div>
+        <div className='start-utility'>
+          <AiOutlinePoweroff size={21} />
+          {isHover ? <span>Power</span> : null}
+        </div>
+      </div>
+    </div>
+  );
+}
