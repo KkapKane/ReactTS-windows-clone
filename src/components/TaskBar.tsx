940bf92d453eb64698d0f4464222c454ba043afb
Lisa <lis-zaa@live.com>
2023-01-08 23:18:21 -0600

    pushed clock to right of taskbar, changed to dark mode colors

--- src/components/TaskBar.tsx
+++ src/components/TaskBar.tsx
@@ -6,29 +6,34 @@ import { useState } from "react";
 import "../styles/taskbar.scss";
 import Start from "./start/Start";
 import Task from "./Task";
+import { BiMessageAlt } from 'react-icons/bi';
 
 interface Props {
-  clock: boolean;
-  handleClock: () => void;
+    clock: boolean;
+    handleClock: () => void;
 }
 
 export default function TaskBar({ handleClock, clock }: Props) {
-  const [tasks, setTask] = useState([
-    { name: "Search", icon: search, hover: false },
-    { name: "Task view", icon: taskView, hover: false },
-    { name: "File Explorer", icon: fileExplorer, hover: false },
-  ]);
+    const [tasks, setTask] = useState([
+        { name: "Search", icon: search, hover: false },
+        { name: "Task view", icon: taskView, hover: false },
+        { name: "File Explorer", icon: fileExplorer, hover: false },
+    ]);
 
-  return (
-    <div id='task-bar'>
-      <Start />
-      {tasks.map((task) => {
-        return (
-          <Task task={task} tasks={tasks} key={task.name} setTask={setTask} />
-        );
-      })}
-
-      <SmallClock handleClock={handleClock} clock={clock} />
-    </div>
-  );
+    return (
+        <div id='task-bar'>
+            <div id="task-left">
+                <Start />
+                {tasks.map((task) => {
+                    return (
+                        <Task task={task} tasks={tasks} key={task.name} setTask={setTask} />
+                    );
+                })}
+            </div>
+            <div id="task-right">
+                <SmallClock handleClock={handleClock} clock={clock} />
+                <BiMessageAlt size={23} />
+            </div>
+        </div>
+    );
 }
