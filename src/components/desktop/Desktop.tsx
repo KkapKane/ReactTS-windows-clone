b63349a8ea5831eb6d3c1c700dfd9482e201ce54
Kane <LisaxKane@gmail.com>
2023-01-16 01:08:40 -0600

    added all the programs inside the desktop component now

--- src/components/desktop/Desktop.tsx
+++ src/components/desktop/Desktop.tsx
@@ -1,27 +1,45 @@
 import '../../styles/desktop.scss'
-
+import Paint from "../../components/programs/Paint/Paint";
+import Calculator from "../../components/programs/Calculator";
+import Audition from "../../components/programs/Audition";
 interface Props {
   desktopIcon: {
     name: string;
     icon: string;
   }[];
-  
+  calcRef: React.RefObject<HTMLDivElement>;
+  audiRef: React.RefObject<HTMLDivElement>;
+  paintRef: React.RefObject<HTMLDivElement>;
+  programs: {
+    name: string;
+    visible: boolean;
+  }[];
+  containerRef: React.RefObject<HTMLDivElement>;
 }
 
-export default function Desktop({desktopIcon}: Props){
+export default function Desktop({desktopIcon, paintRef, calcRef, audiRef, programs, containerRef}: Props){
 
 
   
     return (
-        <div id="desktop">
-            {desktopIcon.map((cell, index)=> {
-                return ( 
-                    <div className='desktop-icon'>
-                        <img className='icon' src={desktopIcon[index].icon} alt="" />
-                        {desktopIcon[index].name}
-                    </div>
-                )
-            })}
-        </div>
-    )
+      <div id='desktop'>
+        {programs[0].visible === true ? (
+          <Calculator calcRef={calcRef} containerRef={containerRef} />
+        ) : null}
+        {programs[1].visible === true ? (
+          <Paint paintRef={paintRef} containerRef={containerRef} />
+        ) : null}
+        {programs[2].visible === true ? (
+          <Audition audiRef={audiRef} containerRef={containerRef} />
+        ) : null}
+        {desktopIcon.map((cell, index) => {
+          return (
+            <div className='desktop-icon'>
+              <img className='icon' src={desktopIcon[index].icon} alt='' />
+              {desktopIcon[index].name}
+            </div>
+          );
+        })}
+      </div>
+    );
 }
