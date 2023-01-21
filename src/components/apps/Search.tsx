438bcd4dec11a1562dadbc2b7eeac36d27bd6416
Kane <LisaxKane@gmail.com>
2023-01-21 02:30:20 -0600

    fixed input field position

--- src/components/apps/Search.tsx
+++ src/components/apps/Search.tsx
@@ -1,10 +1,14 @@
 import '../../styles/search.scss';
 import SearchRight from './SearchRight';
-
+import { useState, useEffect } from 'react';
 
 export default function Search() {
 
+    const [input, setInput] = useState('')
 
+    useEffect(()=>{
+        console.log(input)
+    },[input])
 
     return (
         <div id="search" onClick={(e) => e.stopPropagation()}>
@@ -18,7 +22,7 @@ export default function Search() {
                 </div>
                 <SearchRight />
             </div>
-            <input type="text" />
+            <input type="text"  onChange={(e)=> setInput(e.target.value)}/>
         </div>
     )
 }
