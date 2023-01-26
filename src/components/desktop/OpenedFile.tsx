a8c8a41c978a99309699cc46ec0135689e49fb5f
Lisa <lis-zaa@live.com>
2023-01-26 02:19:26 -0600

    several styling changes for fileopened and iframes

--- src/components/desktop/OpenedFile.tsx
+++ src/components/desktop/OpenedFile.tsx
@@ -1,7 +1,9 @@
 import '../../styles/openedFile.scss'
-import{useEffect, useRef, useState, useContext} from 'react';
+import { useEffect, useRef, useState, useContext } from 'react';
 import { dragDrop } from '../../helper/DragDrop';
-import { MdOutlineClose } from 'react-icons/md'
+import { RxCross2 } from 'react-icons/rx';
+import { HiChevronRight } from "react-icons/hi";
+import { FiArrowLeft } from "react-icons/fi";
 import { DesktopIconType } from '../../types/project_types';
 import { Programs } from '../context/Programs';
 import { MouseEvent } from 'react';
@@ -36,137 +38,150 @@ interface Props {
 }
 
 
-export default function OpenedFile({  icon,setCurrentPath, fileContainerInfo, setFileContainerInfo, currentPath, findMouseLocation, containerRef, allFiles, setAllFiles, setInput,inputRef, currentFocus, handleKeyDown}: Props){
-    
-    
-   
-    const fileRef = useRef(null)
-    const isClicked = useRef<boolean>(false);
-    
-    const coords = useRef<{startX: number;startY: number;lastX: number;lastY: number;}>
-    ({startX: 0,startY: 0,lastX: 0,lastY: 0,});
+export default function OpenedFile({ icon, setCurrentPath, fileContainerInfo, setFileContainerInfo, currentPath, findMouseLocation, containerRef, allFiles, setAllFiles, setInput, inputRef, currentFocus, handleKeyDown }: Props) {
 
 
-     
-    const closeFile = () => {
-        let newFile = allFiles.map((x: any)=>{
-            if(x.name == icon.name){
-                return {...x, open: false}
-            }else {
-                return {...x, open: false}
-            }
-        })
-        setCurrentPath([])
-        setAllFiles(newFile)
-    }
 
- 
-    
-    //all system files
-    
-    useEffect(()=>{
-      if(currentPath.includes(icon.name)) return;
-       
-       setCurrentPath((prev)=> ([...prev, icon.name]));
-      
- 
-      
-        // dragDrop(fileRef,containerRef,'handle',coords,isClicked)
-    },[])
-    
-    useEffect(()=>{
-      console.log(currentPath)
-    },[currentPath])
+  const fileRef = useRef(null)
+  const isClicked = useRef<boolean>(false);
 
- const goBack= () =>{
-    if(icon.parent == '') return;
-    let previous = allFiles.map((file: any)=>{
-      if(icon.parent === file.name){
-        return {...file, open: true}
+  const coords = useRef<{ startX: number; startY: number; lastX: number; lastY: number; }>
+    ({ startX: 0, startY: 0, lastX: 0, lastY: 0, });
+
+
+
+  const closeFile = () => {
+    let newFile = allFiles.map((x: any) => {
+      if (x.name == icon.name) {
+        return { ...x, open: false }
       } else {
-        return {...file, open: false}
-      }  
+        return { ...x, open: false }
+      }
+    })
+    setCurrentPath([])
+    setAllFiles(newFile)
+  }
+
+
+
+  //all system files
+
+  useEffect(() => {
+    if (currentPath.includes(icon.name)) return;
+
+    setCurrentPath((prev) => ([...prev, icon.name]));
+
+
+
+    // dragDrop(fileRef,containerRef,'handle',coords,isClicked)
+  }, [])
+
+  useEffect(() => {
+    console.log(currentPath)
+  }, [currentPath])
+
+  const goBack = () => {
+    if (icon.parent == '') return;
+    let previous = allFiles.map((file: any) => {
+      if (icon.parent === file.name) {
+        return { ...file, open: true }
+      } else {
+        return { ...file, open: false }
+      }
     })
     let erasePath = currentPath
     erasePath.pop()
     erasePath.pop()
     setCurrentPath(erasePath)
     setAllFiles(previous)
-   
- }
 
-useEffect(()=>{
- const falseDrag = () =>{
+  }
 
-   setFileContainerInfo({...fileContainerInfo, dragging: false})
- } 
-document.body.addEventListener('mouseup', falseDrag)
-return ()=> document.body.removeEventListener('mouseup', falseDrag)
-},[fileContainerInfo])
+  useEffect(() => {
+    const falseDrag = () => {
 
-//  const dragStart = (e: MouseEvent) =>{
-//   let target = e.target as HTMLDivElement
-//   setFileContainerInfo({...fileContainerInfo, 
-//     diffX: e.screenX - target.getBoundingClientRect().left,
-//     diffY: e.screenY - target.getBoundingClientRect().top,
-//     dragging: true  
-//   })
-  
-//  }
-//  const dragging = (e: MouseEvent)  =>{
-//   if(fileContainerInfo.dragging){
-//     let left = e.screenX - fileContainerInfo.diffX
-//     let top = e.screenY - fileContainerInfo.diffY
-//     setFileContainerInfo({...fileContainerInfo, styles: {left: left, top: top}})
+      setFileContainerInfo({ ...fileContainerInfo, dragging: false })
+    }
+    document.body.addEventListener('mouseup', falseDrag)
+    return () => document.body.removeEventListener('mouseup', falseDrag)
+  }, [fileContainerInfo])
 
-//   }
-//  }  
-//  const dragEnd = (e: MouseEvent)  =>{
-//   setFileContainerInfo({...fileContainerInfo, dragging: false})
-//  }  
- 
+  //  const dragStart = (e: MouseEvent) =>{
+  //   let target = e.target as HTMLDivElement
+  //   setFileContainerInfo({...fileContainerInfo, 
+  //     diffX: e.screenX - target.getBoundingClientRect().left,
+  //     diffY: e.screenY - target.getBoundingClientRect().top,
+  //     dragging: true  
+  //   })
 
-    return (
-      <div
-  
-        style={fileContainerInfo.styles}
-        onMouseDown={(e ) =>
-          dragStart(e, 'handle', setFileContainerInfo, fileContainerInfo)
-        }
-        onMouseMove={(e) =>
-          dragging(e, setFileContainerInfo, fileContainerInfo)
-        }
-     
-        onMouseUp={(e) => dragEnd(e, setFileContainerInfo, fileContainerInfo)}
-        className='opened-file'
-        ref={fileRef}
-        id={icon.name}
-        onMouseEnter={(e) => findMouseLocation(e)}
-      >
-        <div id='handle'>
-          <button onClick={() => goBack()}>back</button>
+  //  }
+  //  const dragging = (e: MouseEvent)  =>{
+  //   if(fileContainerInfo.dragging){
+  //     let left = e.screenX - fileContainerInfo.diffX
+  //     let top = e.screenY - fileContainerInfo.diffY
+  //     setFileContainerInfo({...fileContainerInfo, styles: {left: left, top: top}})
 
+  //   }
+  //  }  
+  //  const dragEnd = (e: MouseEvent)  =>{
+  //   setFileContainerInfo({...fileContainerInfo, dragging: false})
+  //  }  
+
+
+  return (
+    <div
+
+      style={fileContainerInfo.styles}
+      onMouseDown={(e) =>
+        dragStart(e, 'handle', setFileContainerInfo, fileContainerInfo)
+      }
+      onMouseMove={(e) =>
+        dragging(e, setFileContainerInfo, fileContainerInfo)
+      }
+
+      onMouseUp={(e) => dragEnd(e, setFileContainerInfo, fileContainerInfo)}
+      className='opened-file'
+      ref={fileRef}
+      id={icon.name}
+      onMouseEnter={(e) => findMouseLocation(e)}
+    >
+      <div id='handle'>
+        <div className="icon">
           <img src={icon.icon} alt={icon.name} />
-          {icon.name}
-          <MdOutlineClose onClick={() => closeFile()} />
+          <span>{icon.name}</span> 
         </div>
-        <div className='file-path'>{`${currentPath}`}</div>
-        {allFiles
-          ? allFiles.map((file: any) => {
-              if (file.parent === icon.name) {
-                return (
-                  <DesktopIcon
-                    icon={file}
-                    currentFocus={currentFocus}
-                    inputRef={inputRef}
-                    findMouseLocation={findMouseLocation}
-                    handleKeyDown={handleKeyDown}
-                    setInput={setInput}
-                  />
-                );
-              }
-            })
-          : null}
+        <RxCross2 size={20} onClick={() => closeFile()} />
       </div>
-    );
+      <div className="file-navigation">
+        <button onClick={() => goBack()}> <FiArrowLeft size={16} /> </button>
+        <div className='file-path'>
+          {currentPath.map((path: string, index: number) => {
+            return (
+              <span key={index}>
+                <p>{path}</p>
+                <HiChevronRight />
+              </span>
+            )
+          })}
+        </div>
+      </div>
+
+      {allFiles
+        ? allFiles.map((file: any) => {
+          if (file.parent === icon.name) {
+            return (
+              <DesktopIcon
+                icon={file}
+                currentFocus={currentFocus}
+                inputRef={inputRef}
+                findMouseLocation={findMouseLocation}
+                handleKeyDown={handleKeyDown}
+                setInput={setInput}
+              />
+            );
+          }
+        })
+        : null}
+    </div>
+  );
 }
